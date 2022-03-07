26cc9fa1b3c4187277d806f8e0f01726b5b6f7d3
dymafr <erwan1309@hotmail.com>
2022-03-07 15:33:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ prenom }} {{ nom }}</h1>
+  <Modal v-model:prenom="prenom" v-model:nom="nom" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Modal from './Modal.vue';
+
+const prenom = ref('');
+const nom = ref('');
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    type="text"
+    :value="prenom"
+    @input="$emit('update:prenom', $event.target.value)"
+  />
+  <input
+    type="text"
+    :value="nom"
+    @input="$emit('update:nom', $event.target.value)"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  prenom: string;
+  nom: string;
+}>();
+
+defineEmits<{
+  (e: 'update:prenom', value: string);
+  (e: 'update:nom', value: string);
+}>();
+</script>
+
+<style scoped lang="scss"></style>
